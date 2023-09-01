e492349aa51101a9e7c1797a4c04b9e6ebc97b85
Valeria Serna <valeriasernasalazar@icloud.com>
2023-09-01 15:56:12 -0600

    codigo final con colorcitos

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -52,9 +52,9 @@
             label1.AutoSize = true;
             label1.BackColor = Color.Transparent;
             label1.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(296, 23);
+            label1.Location = new Point(338, 31);
             label1.Name = "label1";
-            label1.Size = new Size(458, 32);
+            label1.Size = new Size(579, 41);
             label1.TabIndex = 0;
             label1.Text = "Simulación del Algoritmo de MonteCarlo";
             label1.TextAlign = ContentAlignment.TopCenter;
@@ -62,9 +62,9 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(50, 149);
+            label2.Location = new Point(57, 199);
             label2.Name = "label2";
-            label2.Size = new Size(142, 15);
+            label2.Size = new Size(178, 20);
             label2.TabIndex = 1;
             label2.Text = "Número de Experimentos";
             label2.Click += label2_Click;
@@ -72,9 +72,9 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(68, 218);
+            label3.Location = new Point(78, 291);
             label3.Name = "label3";
-            label3.Size = new Size(110, 15);
+            label3.Size = new Size(137, 20);
             label3.TabIndex = 2;
             label3.Text = "Número de Paneles";
             label3.Click += label3_Click;
@@ -82,9 +82,9 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(81, 294);
+            label4.Location = new Point(93, 392);
             label4.Name = "label4";
-            label4.Size = new Size(81, 15);
+            label4.Size = new Size(102, 20);
             label4.TabIndex = 3;
             label4.Text = "Límite Inferior";
             label4.Click += label4_Click;
@@ -92,42 +92,46 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(80, 360);
+            label5.Location = new Point(91, 480);
             label5.Name = "label5";
-            label5.Size = new Size(87, 15);
+            label5.Size = new Size(110, 20);
             label5.TabIndex = 4;
             label5.Text = "Límite Superior";
             label5.Click += label5_Click;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(72, 167);
+            textBox1.Location = new Point(82, 223);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
+            textBox1.Size = new Size(114, 27);
             textBox1.TabIndex = 5;
             textBox1.TextChanged += textBox1_TextChanged;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(72, 236);
+            textBox2.Location = new Point(82, 315);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(100, 23);
+            textBox2.Size = new Size(114, 27);
             textBox2.TabIndex = 6;
             textBox2.TextChanged += textBox2_TextChanged;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(72, 312);
+            textBox3.Location = new Point(82, 416);
+            textBox3.Margin = new Padding(3, 4, 3, 4);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(100, 23);
+            textBox3.Size = new Size(114, 27);
             textBox3.TabIndex = 7;
             textBox3.TextChanged += textBox3_TextChanged;
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(72, 379);
+            textBox4.Location = new Point(82, 505);
+            textBox4.Margin = new Padding(3, 4, 3, 4);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(100, 23);
+            textBox4.Size = new Size(114, 27);
             textBox4.TabIndex = 8;
             textBox4.TextChanged += textBox4_TextChanged;
             // 
@@ -135,9 +139,10 @@
             // 
             button1.BackColor = Color.LightBlue;
             button1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            button1.Location = new Point(287, 498);
+            button1.Location = new Point(328, 664);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(135, 71);
+            button1.Size = new Size(154, 95);
             button1.TabIndex = 9;
             button1.Text = "Ejecutar Algoritmo";
             button1.UseVisualStyleBackColor = false;
@@ -148,10 +153,12 @@
             dataGridView1.BackgroundColor = SystemColors.Control;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.GridColor = Color.White;
-            dataGridView1.Location = new Point(235, 93);
+            dataGridView1.Location = new Point(269, 124);
+            dataGridView1.Margin = new Padding(3, 4, 3, 4);
             dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
             dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(533, 379);
+            dataGridView1.Size = new Size(663, 505);
             dataGridView1.TabIndex = 10;
             dataGridView1.CellContentClick += dataGridView1_CellContentClick;
             // 
@@ -160,10 +167,12 @@
             dataGridView2.BackgroundColor = SystemColors.Control;
             dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView2.GridColor = SystemColors.Control;
-            dataGridView2.Location = new Point(799, 93);
+            dataGridView2.Location = new Point(979, 124);
+            dataGridView2.Margin = new Padding(3, 4, 3, 4);
             dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowHeadersWidth = 51;
             dataGridView2.RowTemplate.Height = 25;
-            dataGridView2.Size = new Size(135, 379);
+            dataGridView2.Size = new Size(154, 505);
             dataGridView2.TabIndex = 11;
             dataGridView2.CellContentClick += dataGridView2_CellContentClick;
             // 
@@ -171,18 +180,20 @@
             // 
             dataGridView3.BackgroundColor = SystemColors.Control;
             dataGridView3.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView3.Location = new Point(546, 498);
+            dataGridView3.Location = new Point(690, 664);
+            dataGridView3.Margin = new Padding(3, 4, 3, 4);
             dataGridView3.Name = "dataGridView3";
+            dataGridView3.RowHeadersWidth = 51;
             dataGridView3.RowTemplate.Height = 25;
-            dataGridView3.Size = new Size(388, 71);
+            dataGridView3.Size = new Size(443, 95);
             dataGridView3.TabIndex = 12;
             dataGridView3.CellContentClick += dataGridView3_CellContentClick;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1019, 606);
+            ClientSize = new Size(1199, 808);
             Controls.Add(dataGridView3);
             Controls.Add(dataGridView2);
             Controls.Add(dataGridView1);
@@ -196,6 +207,7 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
--- Form1.cs
+++ Form1.cs
@@ -22,21 +22,40 @@ namespace MonteCarloFinal_F
             // segundo if para que los paramaetros de paneles sea maximo 5
             if (Convert.ToInt32(textBox2.Text) > 6)
             {
-                MessageBox.Show("Número máximo de paneles excedido");
+                MessageBox.Show("Número máximo de paneles excedido, debe estar entre 2 y 6.");
                 return;
             }
 
             // tercer if para que los paramaetros de paneles sea minimo 2
             if (Convert.ToInt32(textBox2.Text) < 2)
             {
-                MessageBox.Show("Número de paneles incompleto");
+                MessageBox.Show("El número de paneles debe de estar entre 2 y 6.");
+                return;
+            }
+
+            // Verificar que el num de experimentos sea mayor a uno
+            if(Convert.ToInt32(textBox1.Text) <=0 )
+            {
+                MessageBox.Show("El número de experimentos debe de ser un número positivo y mayor a 0.");
+                return;
+            }
+
+            if (Convert.ToInt32(textBox3.Text) == Convert.ToInt32(textBox4.Text))
+            {
+                MessageBox.Show("El límite inferior y superior deben de ser diferentes.");
+                return;
+            }
+
+            if (Convert.ToInt32(textBox3.Text) < 0 || Convert.ToInt32(textBox4.Text) <0)
+            {
+                MessageBox.Show("Los límites deben de ser positivos");
                 return;
             }
 
             int rows = Convert.ToInt32(textBox1.Text);
             int col = Convert.ToInt32(textBox2.Text);
             double lim_inf = Convert.ToDouble(textBox3.Text);
-            double lim_sup = Convert.ToDouble(textBox4.Text);
+            double lim_sup = Convert.ToDouble(textBox4.Text); 
 
 
             // se genera la matriz con numeros random
@@ -45,11 +64,12 @@ namespace MonteCarloFinal_F
             (List<double> PanelSeleccionado, string algo) = montecarlo.Seleccion(matrix, rows, col);
             (List<double> estadisticos, string cosas) = montecarlo.Calculos(PanelSeleccionado);
 
-            llenarGrid(matrix, col, rows);
+            llenarGrid(matrix, col, rows, PanelSeleccionado);
             llenarGrid2(PanelSeleccionado, rows);
             llenarGrid3(estadisticos);
+
         }
-        public void llenarGrid(double[,] matrix, int col, int rows)
+        public void llenarGrid(double[,] matrix, int col, int rows, List<double> PanelSeleccionado)
         {
             dataGridView1.Columns.Clear();
 
@@ -61,6 +81,12 @@ namespace MonteCarloFinal_F
             {
                 dataGridView1.Columns[j].HeaderText = ($"Panel  #{j + 1}");
             }
+
+            // Set header style
+            dataGridView1.EnableHeadersVisualStyles = false;
+            dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(0,51,102);
+            dataGridView1.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+
 
             // agregar filas a la matriz
             for (int i = 0; i < matrix.GetLength(0); i++)
@@ -75,12 +101,32 @@ namespace MonteCarloFinal_F
                 dataGridView1.Rows[i].HeaderCell.Value = i + 1;
             }
 
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                int index = 0;
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    if (PanelSeleccionado.Contains(Convert.ToDouble(cell.Value)))
+                    {
+                        cell.Style.BackColor = Color.FromArgb(153, 204, 255); ;
+                        break;
+                    }
+                    index++;
+                }
+            }
+
+            dataGridView1.CurrentCell = null;
+
             // agregar control al dataGrid
             this.Controls.Add(dataGridView1);
         }
 
         public void llenarGrid2(List<double> PanelSeleccionado, int rows)
         {
+            dataGridView2.EnableHeadersVisualStyles = false;
+            dataGridView2.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(0, 51, 102);
+            dataGridView2.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+
             dataGridView2.ColumnCount = 1;
             dataGridView2.Rows.Clear();
             dataGridView2.Columns[0].HeaderText = "Seleccion";
@@ -88,10 +134,17 @@ namespace MonteCarloFinal_F
             {
                 dataGridView2.Rows.Add(item);
             }
+            dataGridView2.CurrentCell = null;
+
         }
 
         public void llenarGrid3(List<double> estadisticos)
         {
+            dataGridView3.EnableHeadersVisualStyles = false;
+            dataGridView3.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(0, 51, 102);
+            dataGridView3.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+
+
             dataGridView3.ColumnCount = 3;
             dataGridView3.Rows.Clear();
             dataGridView3.Rows.Add(); // agregar solo una fila
@@ -103,6 +156,9 @@ namespace MonteCarloFinal_F
             {
                 dataGridView3.Rows[0].Cells[i].Value = estadisticos[i];
             }
+
+            dataGridView3.CurrentCell = null;
+
         }
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
@@ -158,5 +214,7 @@ namespace MonteCarloFinal_F
         {
 
         }
+
+
     }
 }
